c32253263a33406e914c4aade97a144419046a16
Banee Ishaque K <Baneeishaque@gmail.com>
2024-01-02 00:40:32 +0530

    Format Code

--- workspaceInitialize.bash
+++ workspaceInitialize.bash
@@ -1,81 +1,81 @@
 eval $(gp env -e) &&
-if [ -v BASH_HUB_ACCESS_TOKEN ] && [ -v BASH_HUB_SYSTEM_NAME ];then
-    bashHubConfigurationFolder=$HOME/.bashhub &&
-        mkdir $bashHubConfigurationFolder &&
-        bashHubConfigurationPath=$bashHubConfigurationFolder/config &&
-        printf "[bashhub]\naccess_token = $(echo $BASH_HUB_ACCESS_TOKEN)\nsystem_name = $(echo $BASH_HUB_SYSTEM_NAME)" >$bashHubConfigurationPath &&
-        cd /workspace &&
-        curl -OL https://bashhub.com/setup &&
-        sed -z 's+\n        if ! ../env/bin/bashhub util update_system_info; then\n            # Run setup if we run into any issues updating our system info\n            ../env/bin/bashhub setup\n        fi++' -i setup &&
-        bash setup &&
-        rm setup
-fi &&
-if [ -v GH_TOKEN ];then
-    echo "export GH_TOKEN=$(echo $GH_TOKEN)" >>~/.bashrc
-    echo "GH_TOKEN = $GH_TOKEN"
-fi &&
-if [ -v GH_TOKEN ] && [ -v GETGIST_USER ];then
-    echo "export GETGIST_TOKEN=$(echo $GH_TOKEN)" >>~/.bashrc &&
-        echo "export GETGIST_USER=$(echo $GETGIST_USER)" >>~/.bashrc
-fi &&
-if [ -v DOCKER_HUB_USERNAME ] && [ -v DOCKER_HUB_PASSWORD ];then
-    echo "export DOCKER_HUB_USERNAME=$(echo $DOCKER_HUB_USERNAME)" >>~/.bashrc &&
-        echo "export DOCKER_HUB_PASSWORD=$(echo $DOCKER_HUB_PASSWORD)" >>~/.bashrc &&
-        docker login --username $(echo $DOCKER_HUB_USERNAME) --password $(echo $DOCKER_HUB_PASSWORD)
-fi &&
-if [ ! -d vscode-insider-user-data ];then
-    mkdir vscode-insider-user-data
-fi &&
-ln -s vscode-insider-user-data "$HOME/.config/Code - Insiders" &&
-mkdir ~/.vscode-insiders &&
-if [ ! -d vscode-insider-extensions ];then
-    mkdir vscode-insider-extensions
-fi &&
-ln -s vscode-insider-extensions ~/.vscode-insiders/extensions &&
-if [ ! -d Periodic-Mouse-Click-Chrome-Selenium-Python ];then
-    git clone https://github.com/Baneeishaque/Periodic-Mouse-Click-Chrome-Selenium-Python.git
-fi &&
-cd Periodic-Mouse-Click-Chrome-Selenium-Python &&
+    if [ -v BASH_HUB_ACCESS_TOKEN ] && [ -v BASH_HUB_SYSTEM_NAME ]; then
+        bashHubConfigurationFolder=$HOME/.bashhub &&
+            mkdir $bashHubConfigurationFolder &&
+            bashHubConfigurationPath=$bashHubConfigurationFolder/config &&
+            printf "[bashhub]\naccess_token = $(echo $BASH_HUB_ACCESS_TOKEN)\nsystem_name = $(echo $BASH_HUB_SYSTEM_NAME)" >$bashHubConfigurationPath &&
+            cd /workspace &&
+            curl -OL https://bashhub.com/setup &&
+            sed -z 's+\n        if ! ../env/bin/bashhub util update_system_info; then\n            # Run setup if we run into any issues updating our system info\n            ../env/bin/bashhub setup\n        fi++' -i setup &&
+            bash setup &&
+            rm setup
+    fi &&
+    if [ -v GH_TOKEN ]; then
+        echo "export GH_TOKEN=$(echo $GH_TOKEN)" >>~/.bashrc
+        echo "GH_TOKEN = $GH_TOKEN"
+    fi &&
+    if [ -v GH_TOKEN ] && [ -v GETGIST_USER ]; then
+        echo "export GETGIST_TOKEN=$(echo $GH_TOKEN)" >>~/.bashrc &&
+            echo "export GETGIST_USER=$(echo $GETGIST_USER)" >>~/.bashrc
+    fi &&
+    if [ -v DOCKER_HUB_USERNAME ] && [ -v DOCKER_HUB_PASSWORD ]; then
+        echo "export DOCKER_HUB_USERNAME=$(echo $DOCKER_HUB_USERNAME)" >>~/.bashrc &&
+            echo "export DOCKER_HUB_PASSWORD=$(echo $DOCKER_HUB_PASSWORD)" >>~/.bashrc &&
+            docker login --username $(echo $DOCKER_HUB_USERNAME) --password $(echo $DOCKER_HUB_PASSWORD)
+    fi &&
+    if [ ! -d vscode-insider-user-data ]; then
+        mkdir vscode-insider-user-data
+    fi &&
+    ln -s vscode-insider-user-data "$HOME/.config/Code - Insiders" &&
+    mkdir ~/.vscode-insiders &&
+    if [ ! -d vscode-insider-extensions ]; then
+        mkdir vscode-insider-extensions
+    fi &&
+    ln -s vscode-insider-extensions ~/.vscode-insiders/extensions &&
+    if [ ! -d Periodic-Mouse-Click-Chrome-Selenium-Python ]; then
+        git clone https://github.com/Baneeishaque/Periodic-Mouse-Click-Chrome-Selenium-Python.git
+    fi &&
+    cd Periodic-Mouse-Click-Chrome-Selenium-Python &&
     git pull &&
     pip install -r requirements.txt &&
     cd .. &&
-if [ -d configurations-private ];then
-    cd configurations-private &&
-    git pull &&
-    cd ..
-else
-    if [ -v CONFIGURATION_REPOSITORY_URL ];then
-        git clone $(echo $CONFIGURATION_REPOSITORY_URL)
-    fi
-fi &&
-if [ -v EDGE_CONFIGURATION_REPOSITORY_URL ];then
-    if [ -d microsoft-edge-config-private ];then
-        cd microsoft-edge-config-private &&
-        git pull &&
-        cd ..
+    if [ -d configurations-private ]; then
+        cd configurations-private &&
+            git pull &&
+            cd ..
     else
-        git clone $(echo $EDGE_CONFIGURATION_REPOSITORY_URL)
+        if [ -v CONFIGURATION_REPOSITORY_URL ]; then
+            git clone $(echo $CONFIGURATION_REPOSITORY_URL)
+        fi
     fi &&
-        rm -rf ~/.config/microsoft-edge-dev &&
-        ln -s microsoft-edge-config-private/microsoft-edge-dev ~/.config/microsoft-edge-dev
-fi &&
-if [ ! -d Android/Sdk ];then
-    androidCommandLineToolsLinuxDownloadUrl="https://dl.google.com/android/repository/$(wget -O - "https://developer.android.com/studio#command-tools" | pup '[data-modal-dialog-id="sdk_linux_download"] text{}')" &&
-    wget $androidCommandLineToolsLinuxDownloadUrl &&
-    androidCommandLineToolsArchieve=$(basename $androidCommandLineToolsLinuxDownloadUrl) &&
-    unzip $androidCommandLineToolsArchieve &&
-    mkdir -p Android/Sdk/cmdline-tools/latest &&
-    mv cmdline-tools/* Android/Sdk/cmdline-tools/latest/ &&
-    rmdir cmdline-tools/ &&
-    rm $androidCommandLineToolsArchieve &&
-    yes | /workspace/Android/Sdk/cmdline-tools/latest/bin/sdkmanager --licenses
-fi &&
-if [ -d fvm/versions/master ];then
-    cd fvm/versions/master &&
-    git pull &&
-    cd /workspace
-fi &&
-. /home/gitpod/.sdkman/bin/sdkman-init.sh &&
+    if [ -v EDGE_CONFIGURATION_REPOSITORY_URL ]; then
+        if [ -d microsoft-edge-config-private ]; then
+            cd microsoft-edge-config-private &&
+                git pull &&
+                cd ..
+        else
+            git clone $(echo $EDGE_CONFIGURATION_REPOSITORY_URL)
+        fi &&
+            rm -rf ~/.config/microsoft-edge-dev &&
+            ln -s microsoft-edge-config-private/microsoft-edge-dev ~/.config/microsoft-edge-dev
+    fi &&
+    if [ ! -d Android/Sdk ]; then
+        androidCommandLineToolsLinuxDownloadUrl="https://dl.google.com/android/repository/$(wget -O - "https://developer.android.com/studio#command-tools" | pup '[data-modal-dialog-id="sdk_linux_download"] text{}')" &&
+            wget $androidCommandLineToolsLinuxDownloadUrl &&
+            androidCommandLineToolsArchieve=$(basename $androidCommandLineToolsLinuxDownloadUrl) &&
+            unzip $androidCommandLineToolsArchieve &&
+            mkdir -p Android/Sdk/cmdline-tools/latest &&
+            mv cmdline-tools/* Android/Sdk/cmdline-tools/latest/ &&
+            rmdir cmdline-tools/ &&
+            rm $androidCommandLineToolsArchieve &&
+            yes | /workspace/Android/Sdk/cmdline-tools/latest/bin/sdkmanager --licenses
+    fi &&
+    if [ -d fvm/versions/master ]; then
+        cd fvm/versions/master &&
+            git pull &&
+            cd /workspace
+    fi &&
+    . /home/gitpod/.sdkman/bin/sdkman-init.sh &&
     sdk use java 17.0.9.fx-zulu &&
     # fvm install stable &&
     # fvm install beta &&
@@ -97,4 +97,4 @@ fi &&
     flutter build aar &&
     cd .. &&
     rm -rf my_module &&
-source ~/.bashrc
+    source ~/.bashrc
