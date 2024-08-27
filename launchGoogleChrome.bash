0dab859ce032871099670e9f51af727678b17ebd
Banee Ishaque K <baneeishaque2@gmail.com>
2024-08-27 07:38:35 +0530

    launchGoogleChrome.bash: typo

--- launchGoogleChrome.bash
+++ launchGoogleChrome.bash
@@ -1,13 +1,13 @@
-eval $(gp env -e) &&
-    cd /workspace &&
-    chromeConfigurationFolder=chrome-config &&
-    if [ -d $chromeConfigurationFolder ]; then
-        cd $chromeConfigurationFolder &&
-            git pull
-    else
-        mkdir $chromeConfigurationFolder &&
-            if [ -v GOOGLE_CHROME_CONFIGURATION_HOME_REPO ]; then
-                git clone --depth 1 $(echo $GOOGLE_CHROME_CONFIGURATION_HOME_REPO) $chromeConfigurationFolder
-            fi
-    fi &&
-    export CHROME_CONFIG_HOME=$(pwd)/$chromeConfigurationFolder && google-chrome
+eval $(gp env -e)
+cd /workspace
+chromeConfigurationFolder=chrome-config
+if [ -d $chromeConfigurationFolder ]; then
+    cd $chromeConfigurationFolder
+    git pull
+else
+    mkdir $chromeConfigurationFolder
+    if [ -v GOOGLE_CHROME_CONFIGURATION_HOME_REPO ]; then
+        git clone --depth 1 $(echo $GOOGLE_CHROME_CONFIGURATION_HOME_REPO) $chromeConfigurationFolder
+    fi
+fi
+export CHROME_CONFIG_HOME=$(pwd)/$chromeConfigurationFolder && google-chrome
