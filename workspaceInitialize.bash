b6c497412fdca56dfeb78b75c3ac69d7f1195f19
Banee Ishaque K <baneeishaque2@gmail.com>
2024-08-27 07:47:28 +0530

    chore: Removed Android SDK

--- workspaceInitialize.bash
+++ workspaceInitialize.bash
@@ -62,18 +62,6 @@ eval $(gp env -e) &&
             git clone $(echo $CONFIGURATION_REPOSITORY_URL)
         fi
     fi &&
-    if [ ! -d Android/Sdk ]; then
-        brew install pup &&
-        androidCommandLineToolsLinuxDownloadUrl="https://dl.google.com/android/repository/$(wget -O - "https://developer.android.com/studio#command-tools" | pup '[data-modal-dialog-id="sdk_linux_download"] text{}')" &&
-            wget $androidCommandLineToolsLinuxDownloadUrl &&
-            androidCommandLineToolsArchieve=$(basename $androidCommandLineToolsLinuxDownloadUrl) &&
-            unzip $androidCommandLineToolsArchieve &&
-            mkdir -p Android/Sdk/cmdline-tools/latest &&
-            mv cmdline-tools/* Android/Sdk/cmdline-tools/latest/ &&
-            rmdir cmdline-tools/ &&
-            rm $androidCommandLineToolsArchieve &&
-            yes | /workspace/Android/Sdk/cmdline-tools/latest/bin/sdkmanager --licenses
-    fi &&
     source ~/.bashrc &&
     cd configurations-private &&
     git stash pop &&
