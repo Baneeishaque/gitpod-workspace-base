f30e8ddf70a5707bd730bce51d3f91633df4db48
Banee Ishaque K <baneeishaque2@gmail.com>
2024-08-29 22:05:11 +0530

    launchAria2Daemon.bash: typo

--- launchAria2Daemon.bash
+++ launchAria2Daemon.bash
@@ -3,3 +3,4 @@
 # TODO: Get session file path from conf file
 touch /workspace/aria2_repository/session.txt
 aria2c --conf-path=/workspace/configurations-private/aria2/aria2_linux_gitpod.conf
+# ssh -L 6800:localhost:6800 'gp ssh'
