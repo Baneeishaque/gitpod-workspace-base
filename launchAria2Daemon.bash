efadf2baac8e3533b4ebbbd31b78b3b0f7c2ece8
Banee Ishaque K <baneeishaque2@gmail.com>
2024-08-29 21:26:34 +0530

    chore: Added launchAria2Daemon.bash

---
+++ launchAria2Daemon.bash
@@ -0,0 +1,5 @@
+./installAria2.bash
+./copyConfigurations.bash
+# TODO: Get session file path from conf file
+touch /workspace/aria2_repository/session.txt
+aria2c --conf-path=/workspace/configurations-private/aria2/aria2_linux_gitpod.conf
