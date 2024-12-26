dcaac0b2dfc119071d77bda1eecced15b5583818
Banee Ishaque K <Baneeishaque@gmail.com>
2024-12-26 21:02:11 +0530

    feat: copyConfiguartions.bash

--- copyConfigurations.bash
+++ copyConfigurations.bash
@@ -12,3 +12,16 @@ else
         git clone $(echo $CONFIGURATION_REPOSITORY_URL)
     fi
 fi
+
+cd /workspace
+if [ -d configurations ]; then
+    cd configurations
+        git stash
+        git pull
+        git stash pop
+        cd ..
+else
+    if [ -v PUBLIC_CONFIGURATION_REPOSITORY_URL ]; then
+        git clone $(echo $PUBLIC_CONFIGURATION_REPOSITORY_URL)
+    fi
+fi
--- launchAria2Daemon.bash
+++ launchAria2Daemon.bash
@@ -3,6 +3,7 @@
 ./installAria2.bash
 ./copyConfigurations.bash
 # TODO: Get session file path from conf file
+mkdir /workspace/aria2_repository
 touch /workspace/aria2_repository/session.txt
-aria2c --conf-path=/workspace/configurations-private/aria2/aria2_linux_gitpod.conf
+aria2c --conf-path=/workspace/configurations/aria2/aria2_linux_gitpod.conf
 # ssh -L 6800:localhost:6800 'gp ssh'
