7cd34c830c74298d13b04a96171b1ee61c82b6e1
Banee Ishaque K <Baneeishaque@gmail.com>
2024-12-26 21:46:37 +0530

    feat: Up with Clean

--- installGitKrakenDesktop.bash
+++ installGitKrakenDesktop.bash
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-gitKrakenDownloadUrl="https://release.gitkraken.com/linux/gitkraken-amd64.deb"
-wget $gitKrakenDownloadUrl
-gitKrakenInstallationFile=$(basename $gitKrakenDownloadUrl)
-./updatePackageIndex.bash
-sudo apt install -y ./$gitKrakenInstallationFile
-rm $gitKrakenInstallationFile
+source ./remoteDebInstallHelper.bash
+installRemoteDeb "https://release.gitkraken.com/linux/gitkraken-amd64.deb"
--- installGoogleChrome.bash
+++ installGoogleChrome.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-cd /tmp
-glink="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-wget "$glink"
+source ./remoteDebInstallHelper.bash
+
 ./updatePackageIndex.bash
 sudo apt install -y libasound2-dev \
     libgtk-3-dev \
     libnss3-dev \
 	fonts-noto \
-    fonts-noto-cjk \
-    ./"${glink##*/}"	
-ln -srf /usr/bin/google-chrome /usr/bin/chromium
+    fonts-noto-cjk
+
+installRemoteDeb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
 # Extra chrome tweaks
 ## Disables welcome screen
 t="$HOME/.config/google-chrome/First Run" && sudo -u gitpod mkdir -p "${t%/*}" && sudo -u gitpod touch "$t"
+
 ## Disables default browser prompt
 t="/etc/opt/chrome/policies/managed/managed_policies.json" && mkdir -p "${t%/*}" && printf '{ "%s": %s }\n' DefaultBrowserSettingEnabled false > "$t"
 
