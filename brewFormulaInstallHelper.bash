d3cb7b41d1433a18fd2d5c203df2af64fe1f654f
Banee Ishaque K <Baneeishaque@gmail.com>
2025-04-05 22:51:38 +0530

    feat: installBrewFormulaAfterSystemAppRemoval

--- brewFormulaInstallHelper.bash
+++ brewFormulaInstallHelper.bash
@@ -6,3 +6,19 @@ installBrewFormula() {
     brew install $formula
     ./cleanupHomebrew.bash
 }
+
+installBrewFormulaAfterSystemAppRemoval() {
+    local systemAppPath=$(which $1)
+    if [ -n "$systemAppPath" ]; then
+        if [ -f "$systemAppPath" ]; then
+            echo "Found existing $1 at $systemAppPath. Renaming it for backup."
+            sudo mv "$systemAppPath" "$systemAppPath.bak"
+        else
+            echo "Error: $systemAppPath is not a file."
+            return 1
+        fi
+    else
+        echo "No existing $1 found. Proceeding with installation."
+    fi
+    installBrewFormula $1
+}
