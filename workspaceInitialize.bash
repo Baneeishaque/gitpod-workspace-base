06dfb7c40ef849d6026a4eadaa3c51d4f3146210
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-04 16:54:43 +0530

    refactor(workspaceInitialize.bash): update script to include workspace initialization and PowerShell installation

--- workspaceInitialize.bash
+++ workspaceInitialize.bash
@@ -1,8 +1,9 @@
 source bash_scripts/gitpod_related_scripts/workspaceInitialize.bash
 
-post_initialize() {
-
-    echo "This is post_initialize from ${BASH_SOURCE[0]}"
-}
-
-main_function
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" &&
+# echo "SCRIPT_DIR: $SCRIPT_DIR" &&
+initialize_workspace &&
+echo "This is post_initialize from ${BASH_SOURCE[0]}" &&
+cd "$SCRIPT_DIR" &&
+# pwd
+. "$SCRIPT_DIR/bash_scripts/installPowerShell.bash"
